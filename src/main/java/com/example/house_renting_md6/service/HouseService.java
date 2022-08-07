c9fab23b258458343686c8836f010539df9d4f6d
Nguyễn Bá Tuấn Anh <anhnbt.it@gmail.com>
2022-08-07 17:40:28 +0700

    refactor code
    - Thêm thông báo không cho sửa trạng thái (trống, đang nâng cấp) khi nhà đã được thuê

--- src/main/java/com/example/house_renting_md6/service/HouseService.java
+++ src/main/java/com/example/house_renting_md6/service/HouseService.java
@@ -1,10 +1,13 @@
 package com.example.house_renting_md6.service;
 
+import com.example.house_renting_md6.CustomException;
 import com.example.house_renting_md6.model.House;
 
 
 public interface HouseService extends IService<House> {
     House findLastHouse();
+
+    House save(Long id, House house) throws CustomException;
 
     Iterable<House> findByOwnerId (Long customer_id);
 
---
+++ src/main/java/com/example/house_renting_md6/utils/StringUtils.java
@@ -0,0 +1,7 @@
+package com.example.house_renting_md6.utils;
+
+public class StringUtils {
+    public static boolean isNullOrEmpty(String str) {
+        return str == null || str.equals("");
+    }
+}
