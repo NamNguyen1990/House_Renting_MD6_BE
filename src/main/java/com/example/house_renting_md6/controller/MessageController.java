bb52a0a8648f085bf3307421fdc68499bf3cadf8
Xuan Nam <namxuan.nguyen1990@gmail.com>
2022-08-10 15:42:04 +0700

    done thong bao

---
+++ src/main/java/com/example/house_renting_md6/controller/MessageController.java
@@ -0,0 +1,63 @@
+package com.example.house_renting_md6.controller;
+
+
+import com.example.house_renting_md6.model.Comment;
+import com.example.house_renting_md6.model.House;
+import com.example.house_renting_md6.model.Messagee;
+import com.example.house_renting_md6.model.User;
+import com.example.house_renting_md6.service.impl.HouseServiceImpl;
+import com.example.house_renting_md6.service.impl.MessageServiceImpl;
+import com.example.house_renting_md6.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+@CrossOrigin("*")
+@RequestMapping("/messages")
+public class MessageController {
+
+    @Autowired
+    MessageServiceImpl messageService;
+
+    @Autowired
+    HouseServiceImpl houseService;
+
+    @Autowired
+    UserServiceImpl userService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Messagee>> findAll() {
+        return new ResponseEntity<>(messageService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Messagee>> findById(@PathVariable long id) {
+        return new ResponseEntity<>(messageService.findById(id), HttpStatus.OK);
+
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Messagee> add(@RequestBody Messagee message) {
+        message.setContent("đã có người thuê nhà");
+        messageService.save(message);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/findByUser/{id}")
+    public ResponseEntity<Iterable<Messagee>> findByUser(@PathVariable Long id){
+        Optional<User> user = userService.findById(id);
+        return new ResponseEntity<>(messageService.findAllByUser(user.get()), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Messagee> delete(@PathVariable Long id) {
+        messageService.remove(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/example/house_renting_md6/service/impl/MessageServiceImpl.java
@@ -0,0 +1,52 @@
+package com.example.house_renting_md6.service.impl;
+
+import com.example.house_renting_md6.model.House;
+import com.example.house_renting_md6.model.Messagee;
+import com.example.house_renting_md6.model.User;
+import com.example.house_renting_md6.repository.CommentRepository;
+import com.example.house_renting_md6.repository.MessageRepository;
+import com.example.house_renting_md6.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import javax.mail.Message;
+import java.util.Optional;
+
+@Service
+public class MessageServiceImpl implements MessageService {
+
+    @Autowired
+    MessageRepository messageRepository;
+
+    @Override
+    public Page<Messagee> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Optional<Messagee> findById(Long id) {
+        return messageRepository.findById(id);
+    }
+
+    @Override
+    public void save(Messagee messagee) {
+        messageRepository.save(messagee);
+    }
+
+    @Override
+    public void remove(Long id) {
+        messageRepository.deleteById(id);
+    }
+
+    @Override
+    public Iterable<Messagee> findAll() {
+        return messageRepository.findAll();
+    }
+
+    @Override
+    public Iterable<Messagee> findAllByUser(User user) {
+        return messageRepository.findAllByUser(user);
+    }
+}
